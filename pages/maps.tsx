70b4af7bb14ad11aae564448ad94d05254dfda20
Eduardo Reveles <me@osiux.ws>
2021-03-20 15:32:00 -0600

    Better view for maps

--- pages/maps.tsx
+++ pages/maps.tsx
@@ -1,25 +1,51 @@
+import { useContext } from 'react';
 import Image from 'next/image';
-import tw from 'twin.macro';
+import tw, { styled } from 'twin.macro';
 
 import { Layout } from '@app/components';
 
 import { maps, MapsTypes } from '@app/config/maps';
+import AppContext from '@app/contexts/AppContext';
 
-const Maps = () => (
-	<Layout>
-		<h1>Maps</h1>
+const ButtonsContainer = tw.div`grid grid-cols-4 grid-rows-2 gap-2`;
+const MapButton = styled.button(({ selected }: { selected?: boolean }) => [
+	tw`text-gray-700 py-1 px-5 mt-2 bg-gray-200 rounded-lg focus:outline-none`,
+	selected && tw`bg-gray-700 text-white border-gray-700`,
+]);
 
-		{Object.keys(maps).map((mapKey: MapsTypes) => {
-			const map = maps[mapKey];
+const Maps = () => {
+	const { map, setMap } = useContext(AppContext);
 
-			return (
-				<div key={`map-${mapKey}`}>
-					<h2>{map.alt}</h2>
-					<Image layout="responsive" {...map} />
+	return (
+		<Layout>
+			<h1>Maps</h1>
+
+			<ButtonsContainer>
+				{Object.keys(maps).map((mapKey: MapsTypes) => {
+					const currentMap = maps[mapKey];
+
+					const onClick = () => setMap(mapKey);
+
+					return (
+						<MapButton
+							key={mapKey}
+							selected={map === mapKey}
+							onClick={onClick}
+						>
+							{currentMap.alt}
+						</MapButton>
+					);
+				})}
+			</ButtonsContainer>
+
+			{map && (
+				<div>
+					<h2>{maps[map].alt}</h2>
+					<Image layout="responsive" {...maps[map]} />
 				</div>
-			);
-		})}
-	</Layout>
-);
+			)}
+		</Layout>
+	);
+};
 
 export default Maps;
